15a0d24297944e083c874684baa5dc35002a0da0
AllanBa17 <a01254006@itesm.mx>
2021-09-10 18:09:00 +0000

    PARCIAL

--- assignments/08PuntoCircunferencia/src/exercise.py
+++ assignments/08PuntoCircunferencia/src/exercise.py
@@ -1,7 +1,21 @@
-
+import math
 
 def main():
     # Escribe tu código abajo de esta línea
+    radio = float(input("Introduce el radio: "))
+    x1 = float(input("Introduce x1: "))
+    y1 = float(input("Introduce y1: "))
+    x2 = float(input("Introduce x2: "))
+    y2 = float(input("Introduce y2: "))
+
+    distancia = math.sqrt((x2-x1)**2+(y2-y1)**2)
+    if distancia<radio:
+        print('DENTRO')
+    else:
+        if distancia==radio:
+            print("SOBRE")
+        else:
+            print("FUERA")
    
 
 if __name__ == '__main__':
